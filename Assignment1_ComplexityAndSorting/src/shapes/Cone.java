4b6c6251319625b4c4650436a978f3356abdc843
tienphap1405 <tienphap1405@gmail.com>
2024-10-24 18:05:40 -0600

    Setting Up The classes Structure
    Setting Up abstract class Shape
    - Implement all required abstracts methods to all shapes

--- Assignment1_ComplexityAndSorting/src/shapes/Cone.java
+++ Assignment1_ComplexityAndSorting/src/shapes/Cone.java
@@ -1,6 +1,22 @@
 package shapes;
 
-public class Cone
+public class Cone extends Shape
 {
+    private double radius;
 
+    public Cone(double height, double radius) {
+        super(height);
+        this.radius = radius;
+    }
+
+    @Override
+    public double calcVolume() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double calcBaseArea() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
 }
--- Assignment1_ComplexityAndSorting/src/shapes/Cylinder.java
+++ Assignment1_ComplexityAndSorting/src/shapes/Cylinder.java
@@ -1,6 +1,25 @@
 package shapes;
 
-public class Cylinder
+
+
+public class Cylinder extends Shape
 {
+    private double radius;
+
+    public Cylinder(double height, double radius) {
+        super(height);
+        this.radius = radius;
+    }
+    
+
+    @Override
+    public double calcVolume() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double calcBaseArea() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 
 }
