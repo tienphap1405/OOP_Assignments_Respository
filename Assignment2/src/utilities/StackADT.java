29b31615d76fae6a2240d506aa74598eca928346
tienphap1405 <tienphap1405@gmail.com>
2024-11-07 13:34:00 -0700

    Setting up Structure

--- Assignment2/src/utilities/StackADT.java
+++ Assignment2/src/utilities/StackADT.java
@@ -2,24 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package appDomain;
+package utilities;
 
 /**
  *
  * @author tienp
  */
-public class AppDriver {
-    public static void main(String args[]){
-
-
-
-
-
-
-    }
-    
-    public static void print(Object message){
-        System.out.println(message);
-    }
+public interface StackADT<E> {
     
 }
