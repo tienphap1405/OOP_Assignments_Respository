27a59f04d7de7438540e88074934ddc7fc8ea236
tienphap1405 <tienphap1405@gmail.com>
2024-11-07 13:23:49 -0700

    Assignment 2

---
+++ Assignment2/src/appDomain/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package appDomain;
+
+/**
+ *
+ * @author tienp
+ */
+public class NewClass {
+    public static void AppDriver(String args[]){
+        
+    }
+    
+}
