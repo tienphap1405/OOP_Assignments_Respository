170a2b83f379a0ffc1eca4c224972f0f1a56cfd4
tienphap1405 <tienphap1405@gmail.com>
2024-11-24 00:55:46 -0700

    Update Parser to Print Error

--- Assignment2/src/appDomain/AppDriver.java
+++ Assignment2/src/appDomain/AppDriver.java
@@ -1,38 +1,33 @@
-
 package appDomain;
 
+import exceptions.EmptyQueueException;
 import implementations.*;
 import java.io.FileNotFoundException;
 
-
 /**
- *
- * @author tienp
+ * Main class
+ * @author Tien Phap (Evan) Nguyen, Simon Luna Patiarroy
  */
 public class AppDriver {
-    public static void main(String args[]){
-        
+    public static void main(String args[]) throws EmptyQueueException {
         String XMLSUFFIX = ".xml";
-   
-//        if (!args[0].endsWith(XMLSUFFIX) && args[1] != null) {
-//            System.err.println("Program requires only one argument,please enter an .xml file");
-//            return; 
-//        }
+        if (args.length != 1 || !args[0].endsWith(XMLSUFFIX)) {
+            System.err.println("Program requires only one argument, please enter an .xml file");
+            return; 
+        }
 
-//      String parserPath = args[0];
-        
-        // Attempting Parser stuff...
-        Parser parser = new Parser("sample2.xml"); // parserPath
+        // Use the provided XML file path
+        String parserPath = args[0];
+
+        Parser parser = new Parser(parserPath);
         try {
             parser.parsingXML();
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
         }
-        catch (FileNotFoundException e) {
-            System.err.print(e.getMessage());
-        }
-
     }
-    
-    public static void print(Object message){
+
+    public static void print(Object message) {
         System.out.println(message);
     }       
 }
