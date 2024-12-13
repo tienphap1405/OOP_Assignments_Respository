10c67610ee60d3c14d6f227b324c27cf040d554d
Lunaroy13 <romano_luna@hotmail.com>
2024-12-12 22:53:05 -0700

    Update WordTracker.java

--- Assignment3/src/implementations/WordTracker.java
+++ Assignment3/src/implementations/WordTracker.java
@@ -41,6 +41,7 @@ public class WordTracker {
     
     public void processLine(String line, int lineNumber, String fileName) {
         StringBuilder sb = new StringBuilder();
+        System.out.println(line);
         
         boolean foundStartOfWord = false;
 
@@ -71,7 +72,7 @@ public class WordTracker {
             }
             
             // if there is a word at the end of a line with no punctuation afterwards
-            if (!sb.isEmpty() && character == line.charAt(line.length() - 1)) {
+            if (!sb.isEmpty() && i == line.length() - 1) {
                 buildWord(sb.toString(), lineNumber, fileName);
                 sb.setLength(0);
                 foundStartOfWord = false;
