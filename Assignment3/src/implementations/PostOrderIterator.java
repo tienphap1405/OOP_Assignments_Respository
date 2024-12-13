2aa7cfe55cb3ab0e8b25fe1b84cb8b46400f66f5
tienphap1405 <tienphap1405@gmail.com>
2024-12-12 23:19:45 -0700

    Add tons of javadoc

--- Assignment3/src/implementations/PostOrderIterator.java
+++ Assignment3/src/implementations/PostOrderIterator.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package implementations;
 
 import java.util.NoSuchElementException;
@@ -9,24 +6,47 @@ import java.util.Stack;
 import utilities.Iterator;
 
 /**
- *
+ * The implementation of an post order iterator for a binary search tree.
+ * This iterator traverses the BST in post-order sequence: LRV (left, right, visiting) node
+ * through the entire tree
  * @author tienp
+ * @param <E> The generic type of elements stored in the BST, the element must be comparable
  */
 public class PostOrderIterator<E extends Comparable<? super E>> implements Iterator<E> {
+    
+    /**
+     * A stack to keep track of tree nodes for the post order traversal.
+     */   
     private Stack<BSTreeNode<E>> stack = new Stack<>();
+    /**
+     * the last visited node check the where the state of the traversal when traverse through the tree.
+     */   
     private BSTreeNode<E> lastVisited = null;
 
+    /**
+     * Constructor of the post order iterator with the root node
+     * @param root the root node to iterate through.
+     */
     public PostOrderIterator(BSTreeNode<E> root) {
         if (root != null) {
             stack.push(root);
         }
     }
 
+    /**
+     * Checks if there any more elements to traverse through in the tree
+     * @return true if there are more elements to iterate,false if no elements left to iterate
+     */
     @Override
     public boolean hasNext() {
         return !stack.isEmpty();
     }
 
+    /**
+     * Returns the next element in the post-order traversal of the BST.
+     * @return The next element in the post-order traversal.
+     * @throws NoSuchElementException If there are no more elements to iterate over.
+     */
     @Override
     public E next() {
         while (hasNext()) {
--- Assignment3/src/implementations/PreOrderIterator.java
+++ Assignment3/src/implementations/PreOrderIterator.java
@@ -1,34 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package implementations;
 
+import java.util.NoSuchElementException;
 import java.util.Stack;
 import utilities.Iterator;
 
 /**
- *
+ * The implementation of an pre order iterator for a binary search tree.
+ * This iterator traverses the BST in pre-order sequence: VLR (visiting, left, right) node
+ * through the entire tree
  * @author tienp
+ * @param <E> The generic type of elements stored in the BST, the element must be comparable
  */
 public class PreOrderIterator<E extends Comparable<? super E>> implements Iterator<E> {
+    
+    /**
+     * A stack to keep track of tree nodes for the pre-order traversal.
+     */
     private Stack<BSTreeNode<E>> stack = new Stack<>();
     
+    /**
+     * Constructor of the pre order iterator with a root node
+     * @param root the root node to iterate through
+     */
     public PreOrderIterator(BSTreeNode<E> root) {
         if (root != null) {
             stack.push(root);
         }
     }
 
+    /**
+     * Checks if there any more elements to traverse through in the tree
+     * @return true if there are more elements to iterate,false if no elements left to iterate
+     */
     @Override
     public boolean hasNext() {
         return !stack.isEmpty();
     }
 
+    /**
+     * Returns the next element in the pre-order traversal of the BST.
+     * @return The next element in the pre-order sequence.
+     * @throws NoSuchElementException If there are no more elements to iterate over.
+     */
     @Override
-    public E next() {
+    public E next() throws NoSuchElementException{
         if (!hasNext()) {
-            return null;
+            throw new NoSuchElementException("No more elements in the iterator.");
         }
         BSTreeNode<E> node = stack.pop();
         E result = node.getElement();
